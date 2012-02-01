cbcc0ebe29ab9c36a91ce8c42e3bf518decd9f8c
jmikulasch@arcor.de <jmikulasch@arcor.de@97e61989-2b44-a7b9-65b2-00565267f3cc>
2012-02-01 20:14:27 +0000

    Modified Added basic sound output

--- trunk/untitled/glwidget.h
+++ trunk/untitled/glwidget.h
@@ -1,11 +1,16 @@
 #ifndef GLWIDGET_H
 #define GLWIDGET_H
 
+#include "AL\alut.h"
 
-
+#define NUM_BUFFERS 1
+#define NUM_SOURCES 1
+#define NUM_ENVIRONMENTS 1
 
 #include <QGLWidget>
 #include <QTimer>
+
+#include <cmath>
 
 class GLWidget : public QGLWidget
 {
@@ -17,9 +22,21 @@ public:
     void paintGL();
     void resizeGL(int width, int height);
 
+public slots:
+    void game_mainloop();
+    void sound_mainloop();
+
 private:
-    QTimer timer;
+
+    QTimer *gametimer;
+    QTimer *soundtimer;
     void print_text(GLfloat x, GLfloat y, GLfloat z, char* text);
+    void calculate_frequency();
+    void calculate_rate();
+
+    ALuint	buffer[NUM_BUFFERS];
+    ALuint	source[NUM_SOURCES];
+    ALuint  environment[NUM_ENVIRONMENTS];
 
     
 };
