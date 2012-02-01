2d472b54d39b1a483de0c5e7f94c775e2da928cd
jmikulasch@arcor.de <jmikulasch@arcor.de@97e61989-2b44-a7b9-65b2-00565267f3cc>
2012-02-01 20:12:54 +0000

    Modified
    Include alut.h and initialize alutInit()

--- untitled/main.cpp
+++ untitled/main.cpp
@@ -1,10 +1,13 @@
 #include <QtGui/QApplication>
 #include "mainwindow.h"
 #include <GL/glut.h>
+#include <AL/alut.h>
 
 int main(int argc, char *argv[])
 {
     glutInit(&argc, argv);
+    alutInit (&argc, argv);
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
